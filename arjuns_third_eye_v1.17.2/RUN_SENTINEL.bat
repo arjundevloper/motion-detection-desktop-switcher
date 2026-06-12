796136fbea8f8b1399daf17a3f28341e217a7521
Arjun <arjun.dev.work@gmail.com>
2026-06-13 01:43:54 +0530

    Delete arjuns_third_eye_v1.17.2 directory

--- arjuns_third_eye_v1.17.2/RUN_SENTINEL.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title ARJUN SUS Setup
-echo.
-echo  Installing dependencies...
-pip install -r requirements.txt --quiet
-
-echo  Launching ARJUN SUS...
-:: pythonw = windowless Python, no console appears at all
-start "" pythonw sentinel.py
